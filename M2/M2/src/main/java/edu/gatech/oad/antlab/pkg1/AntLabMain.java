f46e61d405fdc06efd5e4f6e596dab74656bee98
branden14k <brandenbrown14@hotmail.com>
2014-09-03 12:27:19 -0400

    removed my gtid number and put my ga tech email for the antlabmain.java file

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Branden Brown");
-                  toPrint += p5.toString("902890576");
+                  toPrint += p5.toString("bkbtfl33@gatech.edu");
 		  
         System.out.println(toPrint);
         
